f49f025c461b1c8ae5a55f2445ae813037889737
Srivathsan Rajagopal <srraj@nets.eu>
2021-06-23 16:38:38 +0200

    SonarLint_Check

--- src/main/java/com/tidal/interview/tidal/controller/PlayListController.java
+++ src/main/java/com/tidal/interview/tidal/controller/PlayListController.java
@@ -5,14 +5,14 @@ import com.tidal.interview.tidal.data.PlayListTrackDto;
 import com.tidal.interview.tidal.data.Track;
 import com.tidal.interview.tidal.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 public class PlayListController {
